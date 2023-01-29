04edff215f8426ba6811e375282d3ba1ab940baa
Diana Zaitseva <zaytseva7801@gmail.com>
2023-01-29 21:00:34 +0100

    lesson1

---
+++ ProjectsJava/Lesson1/src/com/gmail/zaytseva7801/Main.java
@@ -0,0 +1,12 @@
+package com.gmail.zaytseva7801;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("Cześć świat");
+		System.out.println("Привет мир!");
+
+	}
+
+}
